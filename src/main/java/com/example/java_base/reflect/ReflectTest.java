b6cd9c5006b1034d30b49a2192a988d858e7d54d
zzq <zha393150945@163.com>
2019-07-17 11:58:17 +0800

    反射

--- src/main/java/com/example/java_base/reflect/ReflectTest.java
+++ src/main/java/com/example/java_base/reflect/ReflectTest.java
@@ -1,8 +1,11 @@
 package com.example.java_base.reflect;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.HashSet;
 
 /**
  * @Author: zzq
@@ -10,22 +13,21 @@ import java.lang.reflect.InvocationTargetException;
  * @Date: 2019/5/24 17:54
  */
 public class ReflectTest {
-    public static void main(String [] args){
-        //使用Class类的newInstance方法(反射机制)
+    public static void main(String[] args) throws Exception {
+        /**
+         * 使用Class类的newInstance方法(反射机制)
+         */
         Class c1 = null;
-        try {
-            c1 = Class.forName("com.example.java_base.reflect.Animal");
-            System.out.println(c1);//打印c1，发现值和字节码中的类的名称一样
-            Animal animal = (Animal)c1.newInstance();
-            Field[] fields = c1.getDeclaredFields();
-            for(Field field :fields){
-                if(field.getType().equals(String.class)){
-                    String name = (String)field.get(animal);
-                    System.out.println(name);
-                }
+        c1 = Class.forName("com.example.java_base.reflect.Animal");
+        System.out.println(c1);//打印c1，发现值和字节码中的类的名称一样
+        Animal animal = (Animal) c1.newInstance();
+        Field[] fields = c1.getDeclaredFields();
+        for (Field field : fields) {
+            if (field.getType().equals(String.class)) {
+                String name = (String) field.get(animal);
+                System.out.println(name);
             }
-    public static void main(String [] args) throws Exception{
-
+        }
         /**
          * 通过构造方法的反射创建对象
          * new Animal(age,name);
@@ -36,7 +38,7 @@ public class ReflectTest {
         /**
          * 通过成员方法的反射
          */
-        /*Animal animal3 = new Animal();
+        Animal animal3 = new Animal();
         Method method = Animal.class.getMethod("sayName", String.class);
         System.out.println(method);
         //方法的执行
@@ -45,7 +47,7 @@ public class ReflectTest {
 
         String[] array = new String[]{"123", "234", "345"};
         int[] a = new int[]{1, 2, 3};
-        printObaject(a);*/
+        printObaject(a);
 
         /**
          * set集合是无序集合，不允许有重复数据，存数据慢(需要比对每个数据的hash值是否相等)，取数据比较快
@@ -68,15 +70,15 @@ public class ReflectTest {
         collection.remove(a1);
         System.out.println(collection.size());
 
-
     }
 
     /**
-     *  通过反射拿到对象中的成员变量
-     *  eg.可以反射拿到框架中的配置文件
+     * 通过反射拿到对象中的成员变量
+     * eg.可以反射拿到框架中的配置文件
+     *
      * @throws Exception
      */
-    public void getFieldObj()throws Exception {
+    public void getFieldObj() throws Exception {
         //1、加载类 ，指定类的完全限定名：包名+类名
         Class c1 = Class.forName("com.example.java_base.reflect.Animal");
         System.out.println(c1);//打印c1，发现值和字节码中的类的名称一样
@@ -90,28 +92,18 @@ public class ReflectTest {
             }
         }
 
-            // 使用Constructor类的newInstance方法(反射机制)
-                Constructor<Animal> constructor = Animal.class.getConstructor(String.class);
+        // 使用Constructor类的newInstance方法(反射机制)
+        Constructor<Animal> constructor = Animal.class.getConstructor(String.class);
 
-                constructor.newInstance("张三");
+        constructor.newInstance("张三");
     }
 
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        }catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
-    public void field() throws Exception{
+
+    public void field() throws Exception {
         /**
          * 通过成员变量的反射
          */
-        Animal animal2 = new Animal("dog",3);
+        Animal animal2 = new Animal("dog", 3);
         Field fieldName = animal2.getClass().getField("name");
         System.out.println("fieldName=" + fieldName);
         Object name = fieldName.get(fieldName.toString());
@@ -120,21 +112,20 @@ public class ReflectTest {
 
     /**
      * 数组的反射
+     *
      * @param obj
      */
-    public static void printObaject(Object obj){
+    public static void printObaject(Object obj) {
         Class clazz = obj.getClass();
-        if(clazz.isArray()){
+        if (clazz.isArray()) {
             int length = Array.getLength(obj);
             for (int i = 0; i < length; i++) {
-                System.out.println(Array.get(obj,i));
+                System.out.println(Array.get(obj, i));
             }
-        }else{
+        } else {
             System.out.println(obj);
         }
 
     }
-
-
 
 }

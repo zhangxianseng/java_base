aaeaf7d99d9664f810104472b83191c3eaf9d585
zhangxianseng <zha393150945@163.com>
2019-08-19 23:58:55 +0800

    二分查找算法

--- src/main/java/com/example/java_base/algorithm/OneSelf.java
+++ src/main/java/com/example/java_base/algorithm/OneSelf.java
@@ -5,7 +5,9 @@ public class OneSelf {
 
     public static void main(String[] args) {
 
-        System.out.println(feibolaqi(4));
+        int[] arr = {1, 3, 4, 6, 7,10};
+        System.out.println(recursionBinarySearch(arr,7,0,arr.length-1));
+        System.out.println(binarySearch(arr,7));
     }
 
     // 递归实现阶乘
@@ -22,6 +24,57 @@ public class OneSelf {
             return 1;
         }
         return feibolaqi(n - 1) + feibolaqi(n - 2);
+    }
+
+    /**
+     * 使用递归的二分查找
+     *title:recursionBinarySearch
+     *@param arr 有序数组
+     *@param key 待查找关键字
+     *@return 找到的位置
+     * 【二分查找要求】：1.必须采用顺序存储结构 2.必须按关键字大小有序排列
+     */
+    public static int recursionBinarySearch(int[] arr,int key,int low,int high){
+        if(key < arr[0] || key > arr[arr.length-1] || low > high){
+            return -1;
+        }
+        int middle = (low + high) / 2;			//初始中间位置
+        if(arr[middle] > key){
+            //比关键字大则关键字在左区域
+            return recursionBinarySearch(arr, key,low,middle-1);
+        }else if(arr[middle] < key){
+            //比关键字小则关键字在右区域
+            return recursionBinarySearch(arr, key,middle+1,high);
+        }else {
+            return middle;
+        }
+    }
+
+
+    /**
+     * * 二分查找算法 * *
+     *
+     * @param srcArray
+     *            有序数组 *
+     * @param des
+     *            查找元素 *
+     * @return des的数组下标，没找到返回-1
+     */
+    public static int binarySearch(int[] srcArray, int des){
+
+        int low = 0;
+        int high = srcArray.length-1;
+        while(low <= high) {
+            int middle = (low + high)/2;
+            if(des == srcArray[middle]) {
+                return middle;
+            }else if(des <srcArray[middle]) {
+                high = middle - 1;
+            }else {
+                low = middle + 1;
+            }
+        }
+        return -1;
     }
 
 }
